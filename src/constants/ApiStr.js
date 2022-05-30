19e0c9f2bea519f34467ef7abcf6b00e180df75e
username <phalado@gmail.com>
2022-05-30 12:37:20 -0300

    Add env vars

--- src/constants/ApiStr.js
+++ src/constants/ApiStr.js
@@ -1,5 +1,6 @@
 const ApiStr = {
-  API_URL: "http://localhost:3010/api/v1",
+  API_URL: process.env.REACT_APP_API_URL,
+  // API_URL: "http://localhost:3010/api/v1",
   // API_URL: "https://jogo-de-tabuleiro.herokuapp.com/api/v1",
 };
 
